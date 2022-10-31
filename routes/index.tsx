d26bfd53a8bcab650e1f80c3a3acea46d237e203
ivy <ivy@not-ivy.dev>
2022-10-30 21:19:06 -0700

    add alt text to button

--- routes/index.tsx
+++ routes/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <hr className="my-2 w-2/4 border-none" />
           <a className="mx-auto flex items-center cursor-pointer select-none group" id="underline" href="https://github.com/not-ivy/melon" target="_blank">
             <span className="text-3xl font-light">A <span className="text-red-400">sweet</span> link shortener.</span>
-            <button className="transition-colors ml-4 duration-300 ease-[cubic-bezier(0.55,0,1,0.45)] text-gray-700 group-hover:text-white">
+            <button className="transition-colors ml-4 duration-300 ease-[cubic-bezier(0.55,0,1,0.45)] text-gray-700 group-hover:text-white" alt="View source code">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6">
                 <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
               </svg>
