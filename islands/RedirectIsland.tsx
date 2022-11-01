fe9f57a3b050376775b43c5e90531c8be9b8e7a1
ivy <ivy@not-ivy.dev>
2022-11-01 09:55:42 -0700

    fix redirecting and remove countdown

--- islands/RedirectIsland.tsx
+++ islands/RedirectIsland.tsx
@@ -2,29 +2,22 @@ import { useEffect, useState } from "preact/hooks";
 import Blankslate from "../components/Blankslate.tsx";
 
 export default function RedirectIsland({ target }: { target: string }) {
-  const [countdown, setCountdown] = useState(3);
   const [url, setUrl] = useState("");
 
   useEffect(() => {
-    fetch(`/api/redirect/${target}`)
-      .then((res) => { if (res.status === 404) { window.location.assign('/404'); return; } return res.text(); })
+    fetch('/api/lengthen', { method: 'POST', body: target })
+      .then((res) => {
+        if (res.status !== 200) { window.location.assign(`/${res.status}`); return; }
+        return res.text();
+      })
       .then((data) => setUrl(data!))
       .catch(() => window.location.assign('/500'))
   }, [])
 
   useEffect(() => {
     if (!url) return;
-    setCountdown((prev) => prev - 1);
-    const interval = setInterval(() => {
-      setCountdown((prev) => prev - 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [url]);
-
-  useEffect(() => {
-    if (countdown) return;
     window.location.assign(url);
-  }, [countdown]);
+  }, [url]);
 
   if (!url) return (
     <Blankslate>
@@ -34,7 +27,7 @@ export default function RedirectIsland({ target }: { target: string }) {
 
   return (
     <Blankslate>
-      Redirecting in <span className="font-medium">{countdown}</span> seconds...
+      Redirecting...
     </Blankslate>
   )
 }
