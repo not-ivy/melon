65182d9baf9cf45e70bb1ba8e920bbb1089c8d38
ivy <ivy@not-ivy.dev>
2023-05-02 22:16:01 -0700

    remove redirect island, use handler instead

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -1,17 +1,26 @@
-import { Head } from "$fresh/runtime.ts";
-import { PageProps } from "$fresh/server.ts";
-import RedirectIsland from "../islands/RedirectIsland.tsx";
+import { Handlers } from '$fresh/server.ts';
 
-export default function Redirect(props: PageProps) {
-  return (
-    <>
-      <Head>
-        <title>Redirecting</title>
-        <meta content="Redirecting..." property="og:title" />
-      </Head>
-      <main className="p-4 w-screen h-screen flex justify-center items-center dark:(bg-gray-900 text-white)">
-        <RedirectIsland target={props.params.slug} />
-      </main>
-    </>
-  )
-}
+export const handler: Handlers<string | null> = {
+  async GET(_, ctx) {
+    const { slug } = ctx.params;
+    const resp = await fetch(`${Deno.env.get('DOMAIN')}/api/lengthen`, {
+      method: 'POST',
+      body: slug,
+    });
+    if (resp.status === 404) {
+      return new Response(null, {
+        status: 302,
+        headers: {
+          Location: '/404',
+        },
+      });
+    }
+    const url = await resp.text();
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: url,
+      },
+    });
+  },
+};
